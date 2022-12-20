682947d26a492a3d1c2a279f87ff873686651589
Valkje <loran.knol@gmail.com>
2022-12-20 08:58:38 +0100

    Adapt permutation strategy (temporary)

--- src/bootstrap_tdia.R
+++ src/bootstrap_tdia.R
@@ -109,26 +109,47 @@ subsampled_tdia <- function(subject_mats, n_parts) {
   n_mats <- n_sub * n_parts
   coords <- 1:n_mats
   
+  if (PERMUTE) {
+    # First permute within subjects, then permute the subjects themselves.
+    # Preserves within-subjects correlation.
+    permuted_idx_within <- replicate(n_sub, sample(1:n_parts), simplify = FALSE)
+    permuted_idx_between <- sample(1:n_sub)
+    
+    sub_mats <- map2(subject_mats, permuted_idx_within, ~ .x[.y])
+    sub_mats <- sub_mats[permuted_idx_between]
+  }
+  
   # Produces a 10000*n_parts*n_sub array
   mat <- sapply(sub_mats, function(mats) sapply(mats, as.vector), 
                 simplify = "array")
   dim(mat) <- c(100*100, n_parts*n_sub)
   
-  if (PERMUTE) {
-    # Permuting the columns of mat will permute the ownership of the individual
-    # histogram matrices.
-    permuted_idx <- sample(1:(n_parts*n_sub))
-    mat <- mat[,permuted_idx]
-  }
+  # Old permutation strategy:
+  # if (PERMUTE) {
+  #   # Permuting the columns of mat will permute the ownership of the individual
+  #   # histogram matrices.
+  #   permuted_idx <- sample(1:(n_parts*n_sub))
+  #   mat <- mat[,permuted_idx]
+  # }
   
   cors <- cor(mat)
+  
+  # Set diagonal to 0 instead of 1, so we won't have to take into account the
+  # correlation with the source matrix itself when assessing the highest cor-
+  # relations
+  diag(cors) <- 0
   
   tdia <- matrix(nrow = n_sub, ncol = n_parts)
   
   for (x in coords) {
     # Select the n_parts highest correlations (the first one will be the
     # correlation of source with itself)
-    lowest_top <- sort(cors[x,], decreasing = TRUE)[n_parts]
+    # lowest_top <- sort(cors[x,], decreasing = TRUE)[n_parts]
+    
+    # Select the n_parts-1 highest correlations (the first one will no longer 
+    # be the correlation of source with itself)
+    lowest_top <- sort(cors[x,], decreasing = TRUE)[n_parts - 1]
+    
     match_idx <- which(cors[x,] >= lowest_top, arr.ind = TRUE)
     
     # Get subject index from x (calculate row number of the n_sub*n_parts matrix) 
@@ -137,9 +158,24 @@ subsampled_tdia <- function(subject_mats, n_parts) {
     match_idx <- (match_idx-1) %/% n_parts + 1
     
     part_idx <- (x-1) %% n_parts + 1
+    
+    if (PERMUTE) {
+      # Get actual subject number; otherwise we'd be comparing the permuted
+      # subject index to the permuted match index, which is like doing no
+      # permutation at all.
+      sub_idx <- permuted_idx_between[sub_idx]
+      
+      # Do the same for the partition number. It is unlikely to matter much,
+      # but perhaps it is useful for downstream analysis.
+      part_idx <- permuted_idx_within[[sub_idx]][part_idx]
+    }
+    
     # Subtract one from the match count to disregard the match of the source
     # matrix with itself
-    tdia[sub_idx, part_idx] <- (sum(match_idx == sub_idx) - 1) / (n_parts - 1)
+    # tdia[sub_idx, part_idx] <- (sum(match_idx == sub_idx) - 1) / (n_parts - 1)
+    
+    # We no longer need to subtract 1 because we set diag(cors) to 0
+    tdia[sub_idx, part_idx] <- sum(match_idx == sub_idx) / (n_parts - 1)
   }
   
   list("tdia" = tdia, "sampled_idx" = sampled_idx)
@@ -175,7 +211,7 @@ stopCluster(cl)
 out_file <- file.path(dat_dir, "tdias.rda")
 
 if (PERMUTE) {
-  out_file <- file.path(dat_dir, "tdias_permuted.rda")
+  out_file <- file.path(dat_dir, "tdias_permuted_blocked2.rda")
 }
 
 save(tdias, file = out_file)
