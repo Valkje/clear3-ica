89963cd62fc482a18cd68d9907882ef8669c4339
Valkje <loran.knol@gmail.com>
2023-01-25 09:21:07 +0100

    Add autocorrelation error term

--- src/complete_data_models.R
+++ src/complete_data_models.R
@@ -4,6 +4,7 @@
 setwd("~/Documents/Autocorrelation")
 # Base directory for the data set we will create
 dat_dir <- "/project_cephfs/3022017.02/projects/lorkno/data"
+# dat_dir <- "~/HPC_project/data"
 
 source("src/load_dependencies.R")
 
@@ -15,30 +16,49 @@ load(file.path(dat_dir, "imp_multi_tdia_30_blocked.rda"))
 # Before we did the imputation, we had to subtract the means from some of the
 # variables to make it work with the mixed imputation models. We now add them
 # back (at least temporarily).
-imputed_models <- mice::complete(imp_multi, "long") %>%
+# imputed_models <- mice::complete(imp_multi, "long") %>%
+#   select(!NSSIyn) %>%
+#   # mutate(
+#   #   across(.fns = function(x) {
+#   #     if (cur_column() %in% names(centers)) {
+#   #       return(scales[cur_column()] * x + centers[cur_column()])
+#   #     }
+#   #     
+#   #     x
+#   #   })
+#   # ) %>%
+#   group_by(.imp) %>%
+#   # summarize(across(menstrualbleeding:n_stressors, 
+#   #                  ~ list(lmer(.x ~ mean_accuracy + 
+#   #                                medianIKD + 
+#   #                                percent95IKD +
+#   #                                madIKD +
+#   #                                autocorrectRate +
+#   #                                backspaceRate +
+#   #                                totalKeyPresses +
+#   #                                active +
+#   #                                upright +
+#   #                                bed +
+#   #                                (1 | subject)))))
+#   summarize(across(menstrualbleeding:n_stressors, 
+#                    ~ list(lme(.x ~ mean_accuracy + 
+#                                  medianIKD + 
+#                                  percent95IKD +
+#                                  madIKD +
+#                                  autocorrectRate +
+#                                  backspaceRate +
+#                                  totalKeyPresses +
+#                                  active +
+#                                  upright +
+#                                  bed,
+#                               random = ~ 1 | subject,
+#                               correlation = corAR1(),
+#                               data = cur_data()))))
+
+all_imps <- mice::complete(imp_multi, "long") %>%
   select(!NSSIyn) %>%
-  # mutate(
-  #   across(.fns = function(x) {
-  #     if (cur_column() %in% names(centers)) {
-  #       return(scales[cur_column()] * x + centers[cur_column()])
-  #     }
-  #     
-  #     x
-  #   })
-  # ) %>%
   group_by(.imp) %>%
-  summarize(across(menstrualbleeding:n_stressors, 
-                   ~ list(lmer(.x ~ mean_accuracy + 
-                                 medianIKD + 
-                                 percent95IKD +
-                                 madIKD +
-                                 autocorrectRate +
-                                 backspaceRate +
-                                 totalKeyPresses +
-                                 active +
-                                 upright +
-                                 bed +
-                                 (1 | subject)))))
+  nest()
 
 ### Rescaling mean_accuracy, restructuring imputed data sets
 
@@ -52,31 +72,48 @@ imputed_models <- mice::complete(imp_multi, "long") %>%
 
 ### Parallel model estimation
 
-# n_threads <- 10
-# 
-# cl <- makeCluster(n_threads)
-# clusterSetRNGStream(cl, NULL) # Make sure every node uses a different RNG stream
-# 
-# # Set up every worker
-# clusterEvalQ(cl, {
-#   library(lme4)
-#   NULL
-# })
-# 
-# start <- Sys.time()
-# 
-# imputed_models <- parSapply(cl, all_imps$data, function(data) {
-#   glmer(mean_accuracy ~ . - subject - .id + (1 | subject), 
-#         data = data,
-#         family = "binomial",
-#         control = glmerControl("bobyqa",
-#                                optCtrl = list(
-#                                  maxfun = 11 * ncol(data)^2
-#                                )))
-# })
-# 
-# print(Sys.time() - start)
-# 
-# stopCluster(cl)
+n_threads <- 10
 
-save(imputed_models, file = file.path(dat_dir, "imputed_models_tdia_30_blocked_centered_scaled.rda"))
+cl <- makeCluster(n_threads)
+clusterSetRNGStream(cl, NULL) # Make sure every node uses a different RNG stream
+
+# Set up every worker
+clusterEvalQ(cl, {
+  library(dplyr)
+  library(lme4)
+  library(nlme)
+  NULL
+})
+
+start <- Sys.time()
+
+imputed_models <- parSapply(cl, all_imps$data, function(data) {
+  # glmer(mean_accuracy ~ . - subject - .id + (1 | subject),
+  #       data = data,
+  #       family = "binomial",
+  #       control = glmerControl("bobyqa",
+  #                              optCtrl = list(
+  #                                maxfun = 11 * ncol(data)^2
+  #                              )))
+  data %>%
+    summarize(across(menstrualbleeding:n_stressors, 
+                     ~ list(lme(.x ~ mean_accuracy + 
+                                  medianIKD + 
+                                  percent95IKD +
+                                  madIKD +
+                                  autocorrectRate +
+                                  backspaceRate +
+                                  totalKeyPresses +
+                                  active +
+                                  upright +
+                                  bed,
+                                random = ~ 1 | subject,
+                                correlation = corAR1(),
+                                data = cur_data()))))
+})
+
+print(Sys.time() - start)
+
+stopCluster(cl)
+
+save(imputed_models, file = file.path(dat_dir, "imputed_models_autocor.rda"))
